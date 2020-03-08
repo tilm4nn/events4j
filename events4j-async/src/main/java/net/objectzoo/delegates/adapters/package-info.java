39da7ceb32a34835bd7dfc18fbd656577fa01b51
Tilmann Kuhn <tilmann.kuhn@bridging-it.de>
2020-03-09 00:02:15 +0100

    Updated Javadoc

---
+++ events4j-async/src/main/java/net/objectzoo/delegates/adapters/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains support classes that can be used to wrap standard delegates to be asynchronous.
+ */
+package net.objectzoo.delegates.adapters;
---
+++ events4j-async/src/main/java/net/objectzoo/delegates/impl/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains support classes that can be used to implement {@code AsyndAction} or {@code AsyncFunction} delegates.
+ */
+package net.objectzoo.delegates.impl;
